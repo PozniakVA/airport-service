ea968efde5d572af374aa9b7a06f63f99196044a
Valentin Pozniak <waliker448@gmail.com>
2024-08-17 12:35:31 +0300

    fix fields.E306 in Route model

--- airport/admin.py
+++ airport/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
 
-# Register your models here.
+from airport.models import Airport, Airplane, AirplaneType, Route
+
+admin.site.register(Airplane)
+admin.site.register(AirplaneType)
+admin.site.register(Airport)
