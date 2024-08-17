d7d10c5a318590b8f65596e3db96c21bd912fbc0
Valentin Pozniak <waliker448@gmail.com>
2024-08-17 15:58:54 +0300

    add endpoint crew

--- airport/urls.py
+++ airport/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path, include
 from rest_framework import routers
 
-from airport.views import AirplaneViewSet, AirplaneTypeViewSet
+from airport.views import AirplaneViewSet, AirplaneTypeViewSet, CrewViewSet
 
 router = routers.DefaultRouter()
 router.register("airplanes", AirplaneViewSet)
 router.register("airplane_types", AirplaneTypeViewSet)
+router.register("crew", CrewViewSet)
+
 urlpatterns = [
     path("", include(router.urls)),
 ]
--- airport/views.py
+++ airport/views.py
@@ -1,11 +1,11 @@
 from rest_framework import viewsets
 
-from airport.models import Airplane, AirplaneType
+from airport.models import Airplane, AirplaneType, Crew
 from airport.serialiser import (
     AirplaneSerializer,
     AirplaneListSerializer,
     AirplaneDetailSerializer,
-    AirplaneTypeSerializer
+    AirplaneTypeSerializer, CrewSerializer
 )
 
 
@@ -24,3 +24,8 @@ class AirplaneViewSet(viewsets.ModelViewSet):
 class AirplaneTypeViewSet(viewsets.ModelViewSet):
     queryset = AirplaneType.objects.all()
     serializer_class = AirplaneTypeSerializer
+
+
+class CrewViewSet(viewsets.ModelViewSet):
+    queryset = Crew.objects.all()
+    serializer_class = CrewSerializer
