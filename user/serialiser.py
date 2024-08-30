038aceec1916763c156d85aeba120e307ba2f1b0
Valentin Pozniak <waliker448@gmail.com>
2024-08-30 19:02:09 +0300

    use email as username for authentication

--- user/serialiser.py
+++ user/serialiser.py
@@ -1,14 +1,20 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from django.utils.translation import gettext as _
 
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = get_user_model()
-        fields = ("id", "username", "email", "password", "is_staff")
+        fields = ("id", "email", "password", "is_staff")
         read_only_fields = ("id", "is_staff")
         extra_kwargs = {
-            "password": {"write_only": True, "min_length": 7}
+            "password": {
+                "write_only": True,
+                "min_length": 7,
+                "style": {"input_type": "password"},
+                "label": _("Password"),
+            }
         }
         def create(self, validated_data):
             return get_user_model().objects.create_user(**validated_data)
